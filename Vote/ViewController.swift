7581da17187e5f2514889228cb818554bafad4ac
VDAB Cursist <vdabcursist@vdabs-imac-6.local>
2017-01-10 09:30:05 +0100

    Initial Commit

---
+++ Vote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vote
+//
+//  Created by VDAB Cursist on 10/01/17.
+//  Copyright © 2017 kN3TT3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
